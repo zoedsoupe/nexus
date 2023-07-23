cf83695489deb371dff9d1707c19239fe7af1a34
Zoey de Souza Pessanha <zoey.spessanha@gmail.com>
2023-07-23 20:58:13 -0300

    add examples documentation

--- lib/escript/example.ex
+++ lib/escript/example.ex
@@ -1,7 +1,29 @@
 defmodule Escript.Example do
+  @moduledoc """
+  This is an example on how to use `Nexus` with an
+  escript application.
+
+  After defined `:escript` entry into your `project/0` function
+  on `mix.exs` and set the `main_module` option, you can safely
+  define your commands as usual with `defcommand/2`, CLI config
+  and handlers.
+
+  Then you need to call `parse/0` macro, which will inject both
+  `parse/1` and `run/1` function, which the latter you can delegate
+  from the `main/1` escript funciton, as can seen below.
+  """
+
   use Nexus
 
   defcommand :foo, required?: true, type: :string
+
+  @impl true
+  def version, do: "0.1.0"
+
+  @impl true
+  def handle_input(:foo, _args) do
+    IO.puts("Running :foo command...")
+  end
 
   Nexus.parse()
 
--- lib/mix/tasks/example.ex
+++ lib/mix/tasks/example.ex
@@ -1,5 +1,19 @@
 defmodule Mix.Tasks.Example do
-  @moduledoc false
+  @moduledoc """
+  This is a Mix Task example using Nexus.
+  Basically, you can `use` both `Mix.Task` and `Nexus`
+  modules, define your commands as usual with `defcommand/2`
+  and implement others callbacks.
+
+  Then you need to call `Nexus.parse/0`, that will inject
+  both `parse/1` and `run/1` functions into your module.
+  In a `Mix.Task` module, the `run/1` function will supply
+  the behaviour, so you don't need to define it yourself.
+
+  If you need to do other computations inside `Mix.Task.run/1`,
+  then simply define `run/1` by yourself and call `__MODULE__.run/1`
+  when you need it, passing the raw args to it.
+  """
 
   use Mix.Task
   use Nexus
@@ -7,14 +21,14 @@ defmodule Mix.Tasks.Example do
   defcommand :foo, type: :string, required?: false
 
   @impl Nexus.CLI
-  def version, do: "v0.1.0"
+  def version, do: "0.1.0"
 
   @impl Nexus.CLI
   def banner, do: "Hello I'm a test"
 
   @impl Nexus.CLI
-  def handle_input(:foo, args) do
-    IO.puts("Executando #{:foo} com args #{args}...")
+  def handle_input(:foo, _args) do
+    IO.puts("Running :foo command...")
   end
 
   Nexus.parse()
