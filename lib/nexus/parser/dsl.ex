358e4051aec1fe9bc0b5c4f6d0c13d344f5aeb22
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2023-09-10 17:18:06 -0300

    i didn't know what happened

---
+++ lib/nexus/parser/dsl.ex
@@ -0,0 +1,35 @@
+defmodule Nexus.Parser.DSL do
+  @moduledoc """
+  Simple DSL to generate Regex "parsers" for Nexus.
+  """
+
+  def boolean(input) do
+    consume(input, ~r/(true|false)/)
+  end
+
+  def integer(input) do
+    consume(input, ~r/-?\d+/)
+  end
+
+  def float(input) do
+    consume(input, ~r/-?\d+\.\d+/)
+  end
+
+  def string(input) do
+    consume(input, ~r/[a-zA-Z]+/)
+  end
+
+  def literal(input, lit) do
+    consume(input, ~r/\b#{lit}\b/)
+  end
+
+  defp consume(input, regex) do
+    if Regex.match?(regex, input) do
+      cap = hd(Regex.run(regex, input, capture: :first))
+      rest = Regex.replace(regex, input, "")
+      {:ok, {cap, rest}}
+    else
+      {:error, input}
+    end
+  end
+end
