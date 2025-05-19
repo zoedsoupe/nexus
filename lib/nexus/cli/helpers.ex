9117048bce05b11b84cf778fed673054296f79b3
PsGarr <pes.garr@gmail.com>
2025-05-20 01:54:21 +0200

    refactor: helpers.ex

--- lib/nexus/cli/helpers.ex
+++ lib/nexus/cli/helpers.ex
@@ -11,21 +11,38 @@ defmodule Nexus.CLI.Helpers do
   end
 
   @doc """
-  Asks a question and returns the user's input.
-  """
-  def ask(question) do
-    IO.write(question <> " ")
-    IO.gets("") |> String.trim()
-  end
+Asks a question and returns the user's input as a string.
+
+## Examples
+    iex> ask("What's your name?")
+    "John"
+
+    iex> ask("Enter a number:", "> ")
+    "42"
+"""
+@spec ask(question :: String.t()) :: String.t()
+@spec ask(question :: String.t(), prompt_symbol :: String.t()) :: String.t()
+def ask(question, prompt_symbol \\ " ") do
+  IO.gets(question <> prompt_symbol) |> String.trim()
+end
 
   @doc """
   Asks a yes/no question and returns true for yes and false for no.
+
+  ## Options
+  - confirmations: A list of strings that are considered affirmative responses.
+                    Defaulting to ["y", "yes"].
+  - negations: A list of strings that are considered negative responses.
+                Defaulting to `["n", "no"]`.
   """
-  def yes?(question) do
-    case ask(question <> " (y/n)") do
-      "y" -> true
-      "n" -> false
-      _ -> yes?(question)
+  @spec yes?(question :: String.t(), confirmations :: list(String.t()), negations :: list(String.t())) :: boolean
+  def yes?(question, confirmations \\ ["y", "yes"], negations \\ ["n", "no"]) do
+    response = ask(question <> " (y/n)") |> String.downcase()
+
+    cond do
+      response in confirmations -> true
+      response in negations -> false
+      true -> yes?(question, confirmations, negations)
     end
   end
 
